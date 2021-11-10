713dfb8e10f88ff115f1e938fae8890826f5cc46
Ashik-Ahammad <ashik19ahammad@gmail.com>
2021-11-10 19:00:03 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+const { MongoClient } = require('mongodb');
+
+// Middleware
+app.use(cors())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xd7k8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('DB connected success')
+    }
+    finally {
+        //await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello up!')
+})
+
+app.listen(port, () => {
+    console.log(`Listening to UnitedP Server at ${port}`)
+})
