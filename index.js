66d28cd64dd424c9f9478cb9a4dae2b3caf7d70a
Ashik-Ahammad <ashik19ahammad@gmail.com>
2021-11-10 19:15:23 +0600

    2nd

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const port = process.env.PORT || 5000;
 const { MongoClient } = require('mongodb');
 
 // Middleware
-app.use(cors())
+app.use(cors());
+app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xd7k8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -17,7 +18,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
-        console.log('DB connected success')
+        console.log('DB connected successfully')
     }
     finally {
         //await client.close();
